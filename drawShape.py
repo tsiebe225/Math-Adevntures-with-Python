fd554e9acdd7a9ea8c824f04994228947ed64230
tsiebe225 <tsiebe@mymail.mines.edu>
2021-05-11 20:19:28 -0500

    added star

--- drawShape.py
+++ drawShape.py
@@ -11,16 +11,17 @@ def drawShapeRot(turtleImp, numberofRepreats = 20, angleDelta = 18, length = 100
         drawShape(turtleImp,length,sides)
         turtleImp.right(angleDelta)
 
-def drawShapeSpiralOut(turtleImp, numberofRepreats = 20, angleDelta = 18, length = 100, sides = 4, posDelta = 25):
+def drawShapeSpiralOffset(turtleImp, numberofRepreats = 20, angleDelta = 18, length = 100, sides = 4, posDelta = 25):
     for i in range(0,numberofRepreats):
         drawShape(turtleImp,length,sides)
         turtleImp.right(angleDelta)
         turtleImp.forward(posDelta)
 
+
 wn = turtle.Screen()
 
 keith = turtle.Turtle()
 
-drawShapeSpiralOut(keith)
+drawShapeSpiralOffset(keith)
 
 wn.exitonclick()
--- squareTurtle.py
+++ squareTurtle.py
@@ -16,10 +16,52 @@ def triangle(turtleHolder, length = 100):
         turtleHolder.right(120)
         turtleHolder.forward(length)
 
+def drawShape(turtleImp , length = 100,sides = 4):
+    for i in range(0,sides):
+        turtleImp.right(360/sides)
+        turtleImp.forward(length)
+        i=i+1
+
+def drawShapeRot(turtleImp, numberofRepreats = 20, angleDelta = 18, length = 100, sides = 4):
+    for i in range(0,numberofRepreats):
+        drawShape(turtleImp,length,sides)
+        turtleImp.right(angleDelta)
+
+def increasingSizeShapeRotate(turtleImp, numberofRepreats = 20, angleDelta = 18, length = 100,lengthDelta = 20, sides = 4):
+    for i in range(0,numberofRepreats):
+        drawShape(turtleImp,length,sides)
+        length = length + lengthDelta
+        turtleImp.right(angleDelta)
+
+def drawShapeSpiralOffsetIncreasing(turtleImp, numberofRepreats = 20, angleDelta = 18, length = 100, lengthDelta = 20, sides = 4, posDelta = 25):
+    for i in range(0,numberofRepreats):
+        drawShape(turtleImp,length,sides)
+        length = length + lengthDelta
+        turtleImp.right(angleDelta)
+        turtleImp.forward(posDelta)
+
+def star(turtleHolder, length = 100):
+    for i in range(0,5):
+        turtleHolder.forward(length)
+        turtleHolder.right(144)
+
+def starSpiral(turtleHolder, length = 100,lengthDelta = 5, repeats = 20, angleDelta = 18):
+    for i in range(0,repeats):
+        star(turtleHolder,length)
+        length = length + lengthDelta
+        turtleHolder.right(angleDelta)
+
+def starSpiralIncrease(turtleHolder, length = 100,lengthDelta = 5, repeats = 20, angleDelta = 18, angleDeltaIncrease = 5):
+    for i in range(0,repeats):
+        star(turtleHolder,length)
+        length = length + lengthDelta
+        turtleHolder.right(angleDelta)
+        angleDelta = angleDelta + angleDeltaIncrease
+
 wn = turtle.Screen()
 
 keith = turtle.Turtle()
 
-triangle(keith, 200)
+starSpiral(keith,150,5,60,3)
 
 wn.exitonclick()
